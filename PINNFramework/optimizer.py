942bf86a4265522c1758dedfdde6a1b04b6b49c1
nih23 <nico.hoffmann@tu-dresden.de>
2020-10-02 08:42:22 +0200

    added some state-of-the-art optimizers

---
+++ PINNFramework/optimizer.py
@@ -0,0 +1,198 @@
+import torch
+import torch.optim as optim
+from torch.optim.optimizer import Optimizer, required
+
+
+
+class Lamb(Optimizer):
+    r"""Implements Lamb algorithm.
+    It has been proposed in `Large Batch Optimization for Deep Learning: Training BERT in 76 minutes`_.
+    Arguments:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float, optional): learning rate (default: 1e-3)
+        betas (Tuple[float, float], optional): coefficients used for computing
+            running averages of gradient and its square (default: (0.9, 0.999))
+        eps (float, optional): term added to the denominator to improve
+            numerical stability (default: 1e-8)
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+        adam (bool, optional): always use trust ratio = 1, which turns this into
+            Adam. Useful for comparison purposes.
+    .. _Large Batch Optimization for Deep Learning: Training BERT in 76 minutes:
+        https://arxiv.org/abs/1904.00962
+    """
+
+    def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-6,
+                 weight_decay=0, adam=False):
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {}".format(eps))
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError("Invalid beta parameter at index 1: {}".format(betas[1]))
+        defaults = dict(lr=lr, betas=betas, eps=eps,
+                        weight_decay=weight_decay)
+        self.adam = adam
+        super(Lamb, self).__init__(params, defaults)
+
+    def step(self, closure=None):
+        """Performs a single optimization step.
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                if grad.is_sparse:
+                    raise RuntimeError('Lamb does not support sparse gradients, consider SparseAdam instad.')
+
+                state = self.state[p]
+
+                # State initialization
+                if len(state) == 0:
+                    state['step'] = 0
+                    # Exponential moving average of gradient values
+                    state['exp_avg'] = torch.zeros_like(p.data)
+                    # Exponential moving average of squared gradient values
+                    state['exp_avg_sq'] = torch.zeros_like(p.data)
+
+                exp_avg, exp_avg_sq = state['exp_avg'], state['exp_avg_sq']
+                beta1, beta2 = group['betas']
+
+                state['step'] += 1
+
+                # Decay the first and second moment rufing average coefficient
+                # m_t
+                exp_avg.mul_(beta1).add_(1 - beta1, grad)
+                # v_t
+                exp_avg_sq.mul_(beta2).addcmul_(1 - beta2, grad, grad)
+
+                # Paper v3 does not use debiasing.
+                # bias_correction1 = 1 - beta1 ** state['step']
+                # bias_correction2 = 1 - beta2 ** state['step']
+                # Apply bias to lr to avoid broadcast.
+                step_size = group['lr'] # * math.sqrt(bias_correction2) / bias_correction1
+
+                weight_norm = p.data.pow(2).sum().sqrt().clamp(0, 10)
+
+                adam_step = exp_avg / exp_avg_sq.sqrt().add(group['eps'])
+                if group['weight_decay'] != 0:
+                    adam_step.add_(group['weight_decay'], p.data)
+
+                adam_norm = adam_step.pow(2).sum().sqrt()
+                if weight_norm == 0 or adam_norm == 0:
+                    trust_ratio = 1
+                else:
+                    trust_ratio = weight_norm / adam_norm
+                state['weight_norm'] = weight_norm
+                state['adam_norm'] = adam_norm
+                state['trust_ratio'] = trust_ratio
+                if self.adam:
+                    trust_ratio = 1
+
+                p.data.add_(-step_size * trust_ratio, adam_step)
+
+        return loss
+    
+
+""" Layer-wise adaptive rate scaling for SGD in PyTorch! """
+class LARS(Optimizer):
+    r"""Implements layer-wise adaptive rate scaling for SGD.
+    Args:
+        params (iterable): iterable of parameters to optimize or dicts defining
+            parameter groups
+        lr (float): base learning rate (\gamma_0)
+        momentum (float, optional): momentum factor (default: 0) ("m")
+        weight_decay (float, optional): weight decay (L2 penalty) (default: 0)
+            ("\beta")
+        eta (float, optional): LARS coefficient
+        max_epoch: maximum training epoch to determine polynomial LR decay.
+    Based on Algorithm 1 of the following paper by You, Gitman, and Ginsburg.
+    Large Batch Training of Convolutional Networks:
+        https://arxiv.org/abs/1708.03888
+    Example:
+        >>> optimizer = LARS(model.parameters(), lr=0.1, eta=1e-3)
+        >>> optimizer.zero_grad()
+        >>> loss_fn(model(input), target).backward()
+        >>> optimizer.step()
+    """
+    def __init__(self, params, lr=required, momentum=.9,
+                 weight_decay=.0005, eta=0.001, max_epoch=200):
+        if lr is not required and lr < 0.0:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if momentum < 0.0:
+            raise ValueError("Invalid momentum value: {}".format(momentum))
+        if weight_decay < 0.0:
+            raise ValueError("Invalid weight_decay value: {}"
+                             .format(weight_decay))
+        if eta < 0.0:
+            raise ValueError("Invalid LARS coefficient value: {}".format(eta))
+
+        self.epoch = 0
+        defaults = dict(lr=lr, momentum=momentum,
+                        weight_decay=weight_decay,
+                        eta=eta, max_epoch=max_epoch)
+        super(LARS, self).__init__(params, defaults)
+
+    def step(self, epoch=None, closure=None):
+        """Performs a single optimization step.
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+            epoch: current epoch to calculate polynomial LR decay schedule.
+                   if None, uses self.epoch and increments it.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        if epoch is None:
+            epoch = self.epoch
+            self.epoch += 1
+
+        for group in self.param_groups:
+            weight_decay = group['weight_decay']
+            momentum = group['momentum']
+            eta = group['eta']
+            lr = group['lr']
+            max_epoch = group['max_epoch']
+
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+
+                param_state = self.state[p]
+                d_p = p.grad.data
+
+                weight_norm = torch.norm(p.data)
+                grad_norm = torch.norm(d_p)
+
+                # Global LR computed on polynomial decay schedule
+                decay = (1 - float(epoch) / max_epoch) ** 2
+                global_lr = lr * decay
+
+                # Compute local learning rate for this layer
+                local_lr = eta * weight_norm / \
+                    (grad_norm + weight_decay * weight_norm)
+
+                # Update the momentum term
+                actual_lr = local_lr * global_lr
+
+                if 'momentum_buffer' not in param_state:
+                    buf = param_state['momentum_buffer'] = \
+                            torch.zeros_like(p.data)
+                else:
+                    buf = param_state['momentum_buffer']
+                buf.mul_(momentum).add_(actual_lr, d_p + weight_decay * p.data)
+                p.data.add_(-buf)
+
+        return loss
