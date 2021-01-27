5c6c6415b376bc364db391b54dfb814bcf9793fb
stillerpatrick <p.stiller@ebs-z.de>
2021-01-27 09:44:44 +0100

    Consistent naming of the HPM function

--- PINNFramework/HPMLoss.py
+++ PINNFramework/HPMLoss.py
@@ -1,20 +1,20 @@
 from .PDELoss import PDELoss
 
 class HPMLoss(PDELoss):
-    def __init__(self, dataset, derivatives, hpm_model, norm='L2', weight=1.):
+    def __init__(self, dataset, hpm_input, hpm_model, norm='L2', weight=1.):
         """
         Constructor of the HPM loss
 
         Args:
             dataset (torch.utils.Dataset): dataset that provides the residual points
-            derivatives (function): function that calculates the needed PDES for the HPM model. The derivatives function
+            hpm_input(function): function that calculates the needed input for the HPM model. The hpm_input function
             should return a list of tensors, where the last entry is the time_derivative
             hpm_model (torch.nn.Module): model for the HPM, represents the underlying PDE
             norm: Norm used for calculation PDE loss
             weight: Weighting for the loss term
         """
         super(HPMLoss, self).__init__(dataset, None, norm, weight)
-        self.derivatives = derivatives
+        self.hpm_input = hpm_input
         self.hpm_model = hpm_model
 
     def __call__(self, x, model, **kwargs):
@@ -27,6 +27,6 @@ class HPMLoss(PDELoss):
         """
         x.requires_grad = True
         prediction_u = model(x)
-        derivatives = self.derivatives(x, prediction_u)
-        hpm_output = self.hpm_model(derivatives)
-        return self.weight * self.norm(derivatives[:, -1], hpm_output)
+        hpm_input = self.hpm_input(x, prediction_u)
+        hpm_output = self.hpm_model(hpm_input)
+        return self.weight * self.norm(hpm_input[:, -1], hpm_output)
