cafcbef485a50ed3ac0710b0dcc2986fd11a8886
StillerPatrick <p.stiller@ebs-z.de>
2021-06-21 16:36:04 +0200

    merge

--- examples/3D_Wave_Equation/training.py
+++ examples/3D_Wave_Equation/training.py
@@ -191,7 +191,7 @@ if __name__ == "__main__":
         model.cuda()
 
     if args.model == "finger":
-        model = pf.models.FingerNet(numFeatures=300, lb=ic_dataset.lb, ub=ic_dataset.ub, activation=torch.sin)
+        model = pf.models.FingerNet(numFeatures=300,numLayers=args.num_hidden, lb=ic_dataset.lb, ub=ic_dataset.ub, activation=torch.sin)
         model.cuda()
 
     if args.model == "snake":
