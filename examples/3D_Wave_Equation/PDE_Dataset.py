5d682bb3cf2169ad65477c92a1d974ccb9a14d0b
stillerpatrick <p.stiller@ebs-z.de>
2021-06-22 10:03:50 +0200

    Use more dense sampling in laser direction and time

--- examples/3D_Wave_Equation/PDE_Dataset.py
+++ examples/3D_Wave_Equation/PDE_Dataset.py
@@ -3,10 +3,10 @@ import openpmd_api as io
 from torch import Tensor
 from pyDOE import lhs
 from torch.utils.data import Dataset
-
+import openpmd_api as io
 
 class PDEDataset(Dataset):
-    def __init__(self, lb, ub, nf, batch_size, iterative_generation=False):
+    def __init__(self, path, nf, batch_size, iteration, tmax):
         """
         Constructor of the PDE Dataset
 
@@ -16,15 +16,40 @@ class PDEDataset(Dataset):
             nf: defines the number of residual points used in total
             batch_size: defines the number of residual points yielded in a batch
         """
-        self.lb = np.array(lb)
-        self.ub = np.array(ub)
-        self.nf = nf
-        self.batch_size = batch_size
-        self.iterative_generation = iterative_generation
 
-        # creating the first sampling strategy which is lhs sampling
-        if not self.iterative_generation:
-            self.xf = lb + (ub - lb) * lhs(4, self.nf)  # creating xf
+
+        self.batch_size = batch_size
+        self.nf = nf
+
+        # loading mesh specifications
+        series = io.Series(path, io.Access_Type.read_only)
+        it = series.iterations[iteration]
+        self.cell_depth = it.get_attribute('cell_depth')
+        self.cell_height = it.get_attribute('cell_height')
+        self.cell_width = it.get_attribute('cell_width')
+
+        e_x = it.meshes["E"]["x"].load_chunk()
+        series.flush()
+
+        field_shape = e_x.shape  # (z, y, x)
+        z_length = field_shape[0]
+        y_length = field_shape[1]
+        x_length = field_shape[2]
+
+        z = np.arange(0, z_length, 2) * self.cell_depth  # reduce sampling in z-direction
+        y = np.arange(0, y_length) * self.cell_height
+        x = np.arange(0, x_length, 2) * self.cell_width  # reduce sampling in x-direction
+        t = np.arange(iteration, tmax)
+
+        Z, Y, X, T = np.meshgrid(z, y, x, t, indexing='ij')
+        z = Z.reshape(-1, 1)
+        x = X.reshape(-1, 1)
+        y = Y.reshape(-1, 1)
+        t = t.reshape(-1, 1)
+        self.samples = np.concatenate([z, y, x, t], axis=1)
+        rand_idx = np.random.choice(self.samples.shape[0], self.nf, replace=False)
+        self.xf = self.samples[rand_idx, :]
+
 
     def __len__(self):
         """
@@ -36,8 +61,5 @@ class PDEDataset(Dataset):
         """
         Yields the batches of xf
         """
-        if not self.iterative_generation:
-            return Tensor(self.xf[item * self.batch_size: (item + 1) * self.batch_size]).float()
-        else:
-            xf = self.lb + (self.ub - self.lb) * lhs(4, self.batch_size)  # creating xf
-            return xf
+        return Tensor(self.xf[item * self.batch_size: (item + 1) * self.batch_size]).float()
+
