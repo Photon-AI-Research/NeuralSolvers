26497ac91fc3c8403a8c6b19044472121245f9a5
stillerpatrick <p.stiller@ebs-z.de>
2021-06-25 13:39:29 +0200

    update on mg

--- examples/3D_Wave_Equation/multi_gpu.sh
+++ examples/3D_Wave_Equation/multi_gpu.sh
@@ -7,7 +7,7 @@
 #SBATCH --gres=gpu:6
 #SBATCH --mem-per-cpu=1443
 #SBATCH -e error_files/experiment_gpinn.txt
-#SBATCH -o outpu_files/experiment_gpinn.txt
+#SBATCH -o output_files/experiment_gpinn.txt
 #SBATCH --reservation=p_da_aipp_292
 #SBATCH -A p_da_aipp
 
@@ -17,6 +17,7 @@ module load PyTorch/1.6.0-fosscuda-2019b-Python-3.7.4
 module load matplotlib/3.1.1-fosscuda-2019b-Python-3.7.4
 module load h5py/2.10.0-fosscuda-2019b-Python-3.7.4
 source ~/neural_solvers/bin/activate
+
 cd /beegfs/global0/ws/s7520458-pinn_wave/NeuralSolvers/examples/3D_Wave_Equation
 srun python training.py --path /beegfs/global0/ws/s7520458-pinn_wave/laser_only/simOutput/openPMD/simData_%T.bp\
                         --iteration 2000\
