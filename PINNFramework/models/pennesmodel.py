53d753864df0a2321b53073670a016f7516032b0
Maksim Zhdanov <50237680+maxxxzdn@users.noreply.github.com>
2021-09-01 15:04:10 +0200

    the last version of the model

--- PINNFramework/models/pennesmodel.py
+++ PINNFramework/models/pennesmodel.py
@@ -1,106 +1,81 @@
-from torch import tanh, tensor, exp, sin, ones
+from torch import randn
 from torch.nn.functional import relu
 from torch.autograd import Variable
 from torch.nn import Module, Parameter
-from numpy import pi as Pi
 
 class PennesHPM(Module):
-
-    def __init__(self, heat_source_net: Module, device = 'cuda'): #k_value: Variable, p_value: Variable, heat_source_net: Module):
-        """
-        Constructor of the multi Model HPM
-        """
+    """
+    Constructor of the Pennes model:
+        du/dt = convection + linear(u) = (a_conv * Δu) + (w * u + b)
+    Args:
+        config (dict): dictionary defining the configuration of the model. 
+            keys: convection, linear_u.
+            values: True (include term in the model) or False (do not include).
+        u_blood (float): arterial blood temperature.
+    """   
+    def __init__(self, config, u_blood = 37.): 
         super().__init__()
-        self.c_1 = 0.12 #mm2/s
-        self.c_2 = 1.
-        self.c_3 = 0.003 #C/s
-        self.u_blood_0 = 37. #C
-        self.heart_rate_value = 0.25 #Hz
-        self.respiration_rate_value = 0.1 #6667 #Hz 
-        self.u_ambient = 21. #C
-        
-        self.heat_source_net = heat_source_net
-                
-        #0.1, 0.01, 0, 1, 1, 0, 0 ,0.01
-        self.a_1 = Parameter(Variable((0.10*ones([640,480]).clone().detach().requires_grad_(True).cuda())))
-        self.a_2 = Parameter(Variable((0.00*ones([640,480]).clone().detach().requires_grad_(True).cuda())))
-        self.a_3 = Parameter(Variable((0.00*ones([640,480]).clone().detach().requires_grad_(True).cuda())))
-        self.a_4 = Parameter(Variable((1.00*ones([640,480]).clone().detach().requires_grad_(True).cuda())))  
-        self.a_5 = Parameter(Variable((1.00*ones([640,480]).clone().detach().requires_grad_(True).cuda())))  
-        self.a_6 = Parameter(Variable((0.00*ones([640,480]).clone().detach().requires_grad_(True).cuda())))
-        self.a_7 = Parameter(Variable((0.00*ones([640,480]).clone().detach().requires_grad_(True).cuda())))
-        self.a_8 = Parameter(Variable((0.01*ones([640,480]).clone().detach().requires_grad_(True).cuda())))
-        self.a_9 = Parameter(Variable((0.10*ones([640,480]).clone().detach().requires_grad_(True).cuda())))
-                   
-    def convection(self, derivatives):        
+        self.config = config
+        self.u_blood = u_blood
+        if config['convection']:
+            self.a_conv = Parameter(Variable((randn([640,480]).clone().detach().requires_grad_(True)))) 
+        if config['linear_u']:
+            self.a_linear_u_w = Parameter(Variable((randn([640,480]).clone().detach().requires_grad_(True))))
+            self.a_linear_u_b = Parameter(Variable((randn([640,480]).clone().detach().requires_grad_(True))))
+            
+    def convection(self, derivatives): 
+        """
+        Convection term of the model:
+            convection = a_conv * Δu
+        It is linear mapping of the convection term in the original equation.
+        Args:
+            derivatives(tensor): tensor of the form [x,y,t,x_indices,y_indices,u,u_xx,u_yy,u_t].
+        """
         u_xx = derivatives[:, 6].view(-1)
         u_yy = derivatives[:, 7].view(-1) 
         x_indices = derivatives[:, 3].view(-1).long()
         y_indices = derivatives[:, 4].view(-1).long()
-        a_5 = self.a_5[x_indices,y_indices].view(-1)
-        a_5 = relu(a_5)
-        return self.c_1 * a_5 * (u_xx + u_yy) 
-    
-    def perfusion(self, derivatives):        
-        u_values = derivatives[:, 5].view(-1)    
-        x_indices = derivatives[:, 3].view(-1).long()
-        y_indices = derivatives[:, 4].view(-1).long()
-        a_1 = self.a_1[x_indices,y_indices].view(-1)
-        a_1 = relu(a_1)
-        return self.c_2 * a_1 * (self.u_blood_0 - u_values)
-    
-    def metabolism(self, derivatives):       
-        u_values = derivatives[:, 5].view(-1)
-        x_indices = derivatives[:, 3].view(-1).long()
-        y_indices = derivatives[:, 4].view(-1).long()
-        a_4 = self.a_4[x_indices,y_indices].view(-1)
-        a_4 = relu(a_4)
-        return self.c_3 * a_4 * exp((u_values-self.u_blood_0)/10)
-    
-    def heat_source(self, derivatives):        
-        heat_source_input = derivatives[:, :3]  # only positional input
-        heat_source_ouput = self.heat_source_net(heat_source_input)
-        heat_source_ouput = heat_source_ouput.view(-1)       
-        return heat_source_ouput
-       
-    def respiration_rate(self, derivatives):
-        t_values = derivatives[:, 2].view(-1)
-        x_indices = derivatives[:, 3].view(-1).long()
-        y_indices = derivatives[:, 4].view(-1).long()
-        a_2 = self.a_2[x_indices,y_indices].view(-1)
-        a_3 = self.a_3[x_indices,y_indices].view(-1)
-        return a_2*sin(2*Pi*self.respiration_rate_value*t_values + a_3) 
+        a_conv = relu(self.a_conv[x_indices,y_indices].view(-1))
+        return a_conv * (u_xx + u_yy) 
 
-    def heart_rate(self, derivatives):
-        t_values = derivatives[:, 2].view(-1)
+    def linear_u(self, derivatives):
+        """
+        Linear term of the model:
+            linear(u) = w * u + b
+        It is linear mapping of the perfusion term in the original equation.
+        Args:
+            derivatives(tensor): tensor of the form [x,y,t,x_indices,y_indices,u,u_xx,u_yy,u_t].
+        """
         x_indices = derivatives[:, 3].view(-1).long()
         y_indices = derivatives[:, 4].view(-1).long()
-        a_6 = self.a_6[x_indices,y_indices].view(-1)
-        a_7 = self.a_7[x_indices,y_indices].view(-1)
-        a_8 = self.a_8[x_indices,y_indices].view(-1)
-        return a_6*sin(2*Pi*self.heart_rate_value*t_values + a_7)
-    
-    def cooling(self, derivatives):
-        x_indices = derivatives[:, 3].view(-1).long()
-        y_indices = derivatives[:, 4].view(-1).long()
-        u_values = derivatives[:, 5].view(-1) 
-        a_9 = self.a_9[x_indices,y_indices].view(-1)
-        a_9 = relu(a_9)
-        return self.c_2 * a_9 * (self.u_ambient - u_values)
+        u_values = derivatives[:, 5].view(-1)
+        a_linear_u_w = self.a_linear_u_w[x_indices,y_indices].view(-1)
+        a_linear_u_b = self.a_linear_u_b[x_indices,y_indices].view(-1)
+        return a_linear_u_w*(u_values-self.u_blood) + a_linear_u_b
 
     def forward(self, derivatives):
         """
-         Derivatives can be interpreted as complete hpm input thus it can also contain u, x, ...
-
-         derivatives = [x,y,t,x_indices,y_indices,u,u_xx,u_yy,u_t]
-        """
-                 
-        predicted_u_t = self.convection(derivatives) + \
-                        self.perfusion(derivatives) + \
-                        self.respiration_rate(derivatives) + \
-                        self.heat_source(derivatives) + \
-                        self.metabolism(derivatives) + \
-                        self.heart_rate(derivatives) + \
-                        self.cooling(derivatives)
- 
+        Forward pass of the model.
+        Args:
+            derivatives(tensor): tensor of the form [x,y,t,x_indices,y_indices,u,u_xx,u_yy,u_t].
+                where x,y,t - spatiotemporal coordinates in physical units;
+                      x_indices, y_indices - grid coordinates;
+                      u, u_xx, u_yy, u_t - temprerature and its derivatives.
+        """               
+        predicted_u_t = 0
+        if self.config['convection']:
+            predicted_u_t += self.convection(derivatives)
+        if self.config['linear_u']:
+            predicted_u_t += self.linear_u(derivatives)
         return predicted_u_t 
+    
+    def cuda(self):
+        """
+        Sends the instance of the class to cuda device.
+        """
+        super().cuda()
+        if self.config['convection']:
+            self.a_conv = self.a_conv.cuda()
+        if self.config['linear_u']:
+            self.a_linear_u_w = self.a_linear_u_w.cuda()
+            self.a_linear_u_b = self.a_linear_u_b.cuda()
