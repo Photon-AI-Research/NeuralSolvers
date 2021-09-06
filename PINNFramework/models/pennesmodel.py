13c2009ffab9e014ee44a359e49127d1e85a8592
Maksim Zhdanov <50237680+maxxxzdn@users.noreply.github.com>
2021-09-06 11:15:04 +0200

    grid coordinates are calculated within

--- PINNFramework/models/pennesmodel.py
+++ PINNFramework/models/pennesmodel.py
@@ -13,10 +13,11 @@ class PennesHPM(Module):
             values: True (include term in the model) or False (do not include).
         u_blood (float): arterial blood temperature.
     """   
-    def __init__(self, config, u_blood = 37.): 
+    def __init__(self, config, u_blood = 37., spat_res = 0.3): 
         super().__init__()
         self.config = config
         self.u_blood = u_blood
+        self.spat_res = 0.3
         if config['convection']:
             self.a_conv = Parameter(Variable((randn([640,480]).clone().detach().requires_grad_(True)))) 
         if config['linear_u']:
@@ -29,12 +30,13 @@ class PennesHPM(Module):
             convection = a_conv * Δu
         It is linear mapping of the convection term in the original equation.
         Args:
-            derivatives(tensor): tensor of the form [x,y,t,x_indices,y_indices,u,u_xx,u_yy,u_t].
+            derivatives(tensor): tensor of the form [x,y,t,u,u_xx,u_yy,u_t].
+                                                    [0,1,2,3,  4 ,  5 , 6 ].
         """
-        u_xx = derivatives[:, 6].view(-1)
-        u_yy = derivatives[:, 7].view(-1) 
-        x_indices = derivatives[:, 3].view(-1).long()
-        y_indices = derivatives[:, 4].view(-1).long()
+        u_xx = derivatives[:, 4].view(-1)
+        u_yy = derivatives[:, 5].view(-1) 
+        x_indices = (derivatives[:, 0].view(-1) / self.spat_res).long()
+        y_indices = (derivatives[:, 1].view(-1) / self.spat_res).long()
         a_conv = relu(self.a_conv[x_indices,y_indices].view(-1))
         return a_conv * (u_xx + u_yy) 
 
@@ -44,11 +46,11 @@ class PennesHPM(Module):
             linear(u) = w * u + b
         It is linear mapping of the perfusion term in the original equation.
         Args:
-            derivatives(tensor): tensor of the form [x,y,t,x_indices,y_indices,u,u_xx,u_yy,u_t].
+            derivatives(tensor): tensor of the form [x,y,t,u,u_xx,u_yy,u_t].
         """
-        x_indices = derivatives[:, 3].view(-1).long()
-        y_indices = derivatives[:, 4].view(-1).long()
-        u_values = derivatives[:, 5].view(-1)
+        x_indices = (derivatives[:, 0].view(-1) / self.spat_res).long()
+        y_indices = (derivatives[:, 1].view(-1) / self.spat_res).long()
+        u_values = derivatives[:, 3].view(-1)
         a_linear_u_w = self.a_linear_u_w[x_indices,y_indices].view(-1)
         a_linear_u_b = self.a_linear_u_b[x_indices,y_indices].view(-1)
         return a_linear_u_w*(u_values-self.u_blood) + a_linear_u_b
@@ -57,9 +59,8 @@ class PennesHPM(Module):
         """
         Forward pass of the model.
         Args:
-            derivatives(tensor): tensor of the form [x,y,t,x_indices,y_indices,u,u_xx,u_yy,u_t].
+            derivatives(tensor): tensor of the form [x,y,t,u,u_xx,u_yy,u_t].
                 where x,y,t - spatiotemporal coordinates in physical units;
-                      x_indices, y_indices - grid coordinates;
                       u, u_xx, u_yy, u_t - temprerature and its derivatives.
         """               
         predicted_u_t = 0
