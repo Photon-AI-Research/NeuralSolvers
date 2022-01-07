07110db0be4c37e20809110706fe6bd40709757a
Jeyhun1 <jeyhun.rustamov@mailbox.tu-dresden.de>
2022-01-07 05:15:39 +0100

    Update PDEloss.py

--- PINNFramework/PDELoss.py
+++ PINNFramework/PDELoss.py
@@ -31,3 +31,37 @@ class PDELoss(LossTerm):
         pde_residual = self.pde(x, u, **kwargs)
         zeros = torch.zeros(pde_residual.shape, device=pde_residual.device)
         return self.norm(pde_residual, zeros)
+
+
+class PDELossAdaptive(PDELoss):
+    def __init__(self, geometry, pde, name, sampler, norm='L2', weight=1.):
+        """
+        Constructor of the PDE Loss
+
+        Args:
+            dataset (torch.utils.Dataset): dataset that provides the residual points
+            pde (function): function that represents residual of the PDE
+            norm: Norm used for calculation PDE loss
+            weight: Weighting for the loss term
+        """
+        super(PDELossAdaptive, self).__init__(geometry, pde, name, norm, weight)
+        self.geometry = geometry
+        self.sampler = sampler
+        #self.pde = pde
+
+    def __call__(self, x: Tensor, model: Module, **kwargs):
+        """
+        Call function of the PDE loss. Calculates the norm of the PDE residual
+
+        x: residual points
+        model: model that predicts the solution of the PDE
+        """
+        #x, weight = self.sampler.sample(domain=self.geometry, model=model, pde=self.pde)
+        x, weight = self.sampler.sample(model=model, pde=self.pde)
+        #print('x', x)
+        x.requires_grad = True  # setting requires grad to true in order to calculate
+        u = model.forward(x)
+        pde_residual = self.pde(x, u, **kwargs)
+        #zeros = torch.zeros(pde_residual.shape, device=pde_residual.device)
+        #return self.norm(pde_residual, zeros)
+        return 1 / self.sampler.num_points * torch.mean(1 / weight * pde_residual ** 2)
--- PINNFramework/Sampler.py
+++ PINNFramework/Sampler.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 from torch import Tensor, ones, stack, load
 from pyDOE import lhs
+import matplotlib.pyplot as plt
 
 class Sampler:
     def __init__(self, geometry, num_points, sampler="lhs"):
@@ -10,6 +11,9 @@ class Sampler:
         self.sampler = sampler
    
     def sample(self, model, pde):
+
+      device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
 
       if self.sampler == "lhs":
         np.random.seed(42)
@@ -45,8 +49,8 @@ class Sampler:
         xf = np.concatenate([random_x, random_t], axis=1)
 
         # make them into tensors
-        xf = torch.tensor(xf).float().cuda()
-        xs = torch.tensor(xs).float().cuda()
+        xf = torch.tensor(xf).float().to(device)
+        xs = torch.tensor(xs).float().to(device)
         
         xs.requires_grad = True
         # predictions seed
@@ -64,6 +68,51 @@ class Sampler:
         self.xf = xf[indicies_new]
         self.weight = q_model[indicies_new]
 
-      return self.xf.cuda(), self.weight.cuda()
+        # plt.figure(figsize=(10,10))
+        # plt.gca().set_title('Adaptive Sampling')
+        # x_domain = np.linspace(lb[0], ub[0], 200)
+        # t_domain = np.linspace(lb[1], ub[1], 200)
+        # X, T = np.meshgrid(x_domain, t_domain)
+        # X = X.reshape(-1,1)
+        # T = T.reshape(-1,1)
+        # #x_field = np.concatenate([X,T],axis=1)
+        # x_field = np.hstack((X.flatten()[:, None], T.flatten()[:, None]))
+
+        # field_prediction = model(torch.tensor(x_field).float().to(device))
+        # field_prediction = field_prediction.detach().cpu().numpy()
+        # field_prediction = field_prediction.reshape(200, 200)
+        # plt.pcolormesh(t_domain, x_domain, field_prediction.T)
+        # #x_train = x_train.cpu().numpy()
+        # xs = xs.detach().cpu().numpy()
+        # plt.scatter(xs[:, 1],xs[:, 0], c='b', marker='x', s=50, alpha=0.5)
+        # #plt.scatter(x_train[:, 0], x_train[:, 1], c='r', marker='x', s=1, alpha=0.5)
+        # xff=self.xf.cpu().numpy()
+        # plt.scatter(xff[:, 1], xff[:, 0], c='r', marker='x', s=50, alpha=0.5)
+        # plt.show()
+        
+
+        plt.figure(figsize=(10,10))
+        t = np.linspace(0,ub[1],200).flatten()[:, None]
+        x = np.linspace(0,ub[0],200).flatten()[:, None]
+        X, T = np.meshgrid(x, t)
+
+        X_star = np.hstack((X.flatten()[:, None], T.flatten()[:, None]))
+        pred = model(Tensor(X_star).to(device))
+        pred_u = pred.detach().cpu().numpy()
+
+        H_pred = pred_u.reshape(X.shape)
+        plt.imshow(H_pred.T, interpolation='nearest', cmap='YlGnBu',
+                    extent= [lb[1], ub[1], lb[0], ub[0]],
+                    origin='lower', aspect='auto')
+        xs = xs.detach().cpu().numpy()
+        plt.scatter(xs[:, 1],xs[:, 0], c='g', marker='x', s=50, alpha=0.5)
+        xff=self.xf.cpu().numpy()
+        plt.scatter(xff[:, 1], xff[:, 0], c='r', marker='x', s=50, alpha=0.5)
+        plt.title('PINN')
+        plt.ylabel('x (cm)')
+        plt.xlabel('t (seconds)')
+        plt.colorbar().set_label('Temperature (°C)')
+        plt.show()
+      return self.xf, self.weight
 
 
