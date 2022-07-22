bbb05a337a0fa9c890ce9d72b43d25e6d7e75151
jeru889b <jeru889b@taurusi2103.taurus.hrsk.tu-dresden.de>
2022-07-22 22:45:50 +0200

    Update Sampler.py

--- PINNFramework/NDCube.py
+++ PINNFramework/NDCube.py
@@ -5,6 +5,18 @@ from .Geometry import Geometry
 
 class NDCube(Geometry):
     def __init__(self, lb, ub, num_points, sampler, num_seed = 1000, device = torch.device("cuda")):
+        """
+        Constructor of the NDCube class
+
+        Args:
+            lb (numpy.ndarray): lower bound of the domain.
+            ub (numpy.ndarray): upper bound of the domain.
+            num_points (int): the number of sampled points.
+            sampler (string): sampling method: "pseudo" (pseudorandom), "LHS" (Latin hypercube sampling), and "adaptive" method.
+            num_seed (int): the number of seed points for adaptive sampling
+            device (torch.device): "cuda" or "cpu".
+        """
+            
         self.num_seed = num_seed 
         super(NDCube, self).__init__(lb, ub, num_points, sampler, device)
 
--- PINNFramework/Sampler.py
+++ PINNFramework/Sampler.py
@@ -2,78 +2,117 @@ import numpy as np
 import torch
 from torch import Tensor, ones, stack, load
 from pyDOE import lhs
-import matplotlib.pyplot as plt
 
 # set initial seed for torch and numpy
-torch.manual_seed(42)
-np.random.seed(42)
-
-class Sampler:
-    def __init__(self, geometry, num_points, ns, sampler="adaptive", device = 'cuda'):
-        """
-        Constructor of the Sampler
-
-        Args:
-            geometry (torch.utils.Dataset): provides the geometry of the domain
-            num_points (int): number of collocation points
-            ns (int): number of seed points
-            sampler (str): type of sampling
-            device: gpu or cpu
-        """
-        self.geometry = geometry
-        self.num_points = num_points
-        self.ns = ns
-        self.sampler = sampler
-        self.device = device
-   
-    def sample(self, model, pde):
-        
-      if self.sampler == "adaptive":
-        lb = self.geometry.xf[0]
-        ub = self.geometry.xf[1]
-
-        # random seeds
-        random_x = np.random.uniform(lb[0], ub[0], self.ns).reshape(-1, 1)
-        random_t = np.random.uniform(lb[1], ub[1], self.ns).reshape(-1, 1)
-
-        xs = np.concatenate([random_x,random_t], axis=1)
-
-        # collocation points    
-        random_x = np.random.uniform(lb[0], ub[0], self.num_points).reshape(-1, 1)
-        random_t = np.random.uniform(lb[1], ub[1], self.num_points).reshape(-1, 1)
-        xf = np.concatenate([random_x, random_t], axis=1)
-
-        # make the points into tensors
-        xf = torch.tensor(xf).float().to(self.device)
-        xs = torch.tensor(xs).float().to(self.device)
-        
-        # prediction with seed points
-        xs.requires_grad = True
-        prediction_seed = model(xs)
-        
-        # pde residual with seed points
-        loss_seed = pde(xs, prediction_seed)
-        losses_xf = torch.zeros_like(xf)
-        
-        # Compute the 2-norm distance between seed points and collocation points
-        dist = torch.cdist(xf, xs, p=2)
-
-        # obtain the smallest element of the given tensor
-        knn = dist.topk(1, largest=False)
-        
-        # assign the seed loss to the loss of the closest collocation points
-        losses_xf = loss_seed[knn.indices[:, 0]]
-
-        # apply softmax function
-        q_model = torch.softmax(losses_xf, dim=0)
-
-        # obtain 'num_points' indices sampled from the multinomial distribution
-        indicies_new = torch.multinomial(q_model[:, 0], self.num_points, replacement=True)
-        
-        # collocation points and corresponding weights
-        self.xf = xf[indicies_new]
-        self.weight = q_model[indicies_new]
-
-      return self.xf, self.weight
+seed = 42
 
 
+def sample(lb, ub, num_points, sampler, num_seed, model, pde, device = torch.device("cuda:0")):
+    """Generate sample points using random, LHS or adaptive sampling methods. Returns either sampled points ("random", "LHS") or sampled points with corresponding weights ("adaptive").
+    Args:
+        lb (numpy.ndarray): lower bound of the domain.
+        ub (numpy.ndarray): upper bound of the domain.
+        num_points (int): the number of sampled points.
+        sampler (string): sampling method: "random" (pseudorandom), "LHS" (Latin hypercube sampling), and "adaptive" method.
+        num_seed (int): the number of seed points for adaptive sampling.
+        model: is the model which is trained to represent the underlying PDE.
+        pde (function): function that represents residual of the PDE.
+        device (torch.device): "cuda" or "cpu".
+    """
+    lb =  lb.reshape(1,-1)
+    ub =  ub.reshape(1,-1)
+    print('device', device)
+    if sampler == "random":
+        return pseudorandom(lb, ub, num_points)
+    if sampler == "LHS":
+        return quasirandom(lb, ub, num_points)
+    if sampler =='adaptive':
+        return adaptive(lb, ub, num_points, num_seed, model, pde, device)
+    raise ValueError(f"{sampler} sampler is not available.")
+    
+    
+def pseudorandom(lb, ub, num_points):
+    """Pseudo random sampling. Returns sampled points in [lb,ub].
+    Args:
+        lb (numpy.ndarray): lower bound of the domain.
+        ub (numpy.ndarray): upper bound of the domain.
+        num_points (int): the number of sampled points.
+    """
+    
+    np.random.seed(seed)
+    dimension = lb.shape[1]
+    xf = np.random.uniform(lb,ub,size=(num_points, dimension))
+    print('xf type', type(xf))
+    return torch.tensor(xf).float()
+
+
+def quasirandom(lb, ub, num_points):
+    """LHS sampling. Returns sampled points in [lb,ub].
+    Args:
+        lb (numpy.ndarray): lower bound of the domain.
+        ub (numpy.ndarray): upper bound of the domain.
+        num_points (int): the number of sampled points.
+    """
+    np.random.seed(seed)
+    dimension = lb.shape[1]
+    xf = lb + (ub - lb) * lhs(dimension, num_points)
+    print('xf type', type(xf))
+    return torch.tensor(xf).float()
+    
+def adaptive(lb, ub, num_points, num_seed, model, pde, device):
+    """Adaptive sampling. Returns concatenated tensor of sampled points in [lb,ub] and corresponding weights.
+    Args:
+        lb (numpy.ndarray): lower bound of the domain.
+        ub (numpy.ndarray): upper bound of the domain.
+        num_points (int): the number of sampled points.
+        num_seed (int): the number of seed points for adaptive sampling.
+        model: is the model which is trained to represent the underlying PDE.
+        pde (function): function that represents residual of the PDE.
+        device (torch.device): "cuda" or "cpu".
+    """
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    dimension = lb.shape[1]
+    xs = np.random.uniform(lb, ub, size=(num_seed, dimension))
+
+    # collocation points
+    xf = np.random.uniform(lb, ub, size=(num_points, dimension))
+
+    # make the points into tensors
+    xf = torch.tensor(xf).float().to(device)
+    xs = torch.tensor(xs).float().to(device)
+
+    # prediction with seed points
+    xs.requires_grad = True
+    prediction_seed = model(xs)
+
+    # pde residual with seed points
+    loss_seed = pde(xs, prediction_seed)
+    losses_xf = torch.zeros_like(xf)
+
+    # Compute the 2-norm distance between seed points and collocation points
+    dist = torch.cdist(xf, xs, p=2)
+
+    # obtain the smallest element of the given tensor
+    knn = dist.topk(1, largest=False)
+
+    # assign the seed loss to the loss of the closest collocation points
+    losses_xf = loss_seed[knn.indices[:, 0]]
+
+    # apply softmax function
+    q_model = torch.softmax(losses_xf, dim=0)
+
+    # obtain 'num_points' indices sampled from the multinomial distribution
+    indicies_new = torch.multinomial(q_model[:, 0], num_points, replacement=True)
+
+    # collocation points and corresponding weights
+    xf = xf[indicies_new]
+    weight = q_model[indicies_new].detach()
+    #print('xf type', type(xf))
+    print('xf ', (xf))
+    print('weight ', (weight))
+    print('xf ', (xf.shape))
+    print('weight ', (weight.shape))
+    #return (xf , weight)
+    return torch.cat((xf,weight), 1)
